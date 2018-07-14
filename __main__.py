3874d81cada8011019de4c778fd50ba4e257374a
Nandan Rao <nandanmarkrao@gmail.com>
2018-07-14 10:46:33 +0200

    Removed conversion of raw messages

--- __main__.py
+++ __main__.py
@@ -16,47 +16,23 @@ def chunk(n, it):
     src = iter(it)
     return takewhile(bool, (list(islice(src, n)) for _ in count(0)))
 
-def get_service_date(entry):
-    report_date = entry['Report_Date']
-    date = entry['Service_Date']
-    date = re.sub(r'[^\d]+', '.', date)
-    date = re.sub(r'^[^\d]', '', date)
-    try:
-        date = datetime.strptime(date, '%d.%m.%Y')
-        if date > report_date:
-            raise FutureException('Service date in future: {}'.format(date))
-    except Exception as e:
-        logging.error(e)
-        date = report_date
-    return date
-
-def prepare_entry(entry):
-    service_date = get_service_date(entry)
-
-    return {
-        'timestamp': entry['Report_Date'],
-        'code': entry['Service_Code'],
-        'serviceDate': service_date,
-        'workerPhone': entry['Sender_Phone_Number'],
-        'patientPhone': entry['Patient_Phone_Number'],
-        'patientName': entry['Patient_Name'],
-        'originalEntry': entry
-    }
-
 def write_results(coll, results):
-    prepared = (prepare_entry(entry) for entry in results)
-    chunked = chunk(20, prepared)
-    checked_keys = ['timestamp', 'workerPhone', 'patientName','code']
+    chunked = chunk(500, results)
+    checked_keys = ['Sender_Phone_Number',
+                    'Patient_Phone_Number',
+                    'Patient_Name',
+                    'Report_Date',
+                    'Service_Code']
     i = 0
     for c in chunked:
         requests = [ UpdateOne({ k: obj[k] for k in checked_keys},
                                { '$setOnInsert': obj },
                                upsert=True) for obj in c]
-        i = i + len(requests)
+        i += len(requests)
         coll.bulk_write(requests, ordered=False)
     return i
 
-# TODO: validate deets...?
+
 mysql_deets = {
     'pass': getenv('MYSQL_PASS'),
     'user': getenv('MYSQL_USER'),
@@ -69,9 +45,10 @@ url = 'mysql+pymysql://{user}:{pass}@{host}:3306/{db}'.format(**mysql_deets)
 
 def get_from(coll):
     try:
-        latest = coll.find().sort('timestamp', -1).limit(1)[0]
-        return latest['timestamp'].isoformat()
-    except:
+        latest = coll.find().sort('Report_Date', -1).limit(1)[0]
+        return latest['Report_Date'].isoformat()
+    except BaseException as e:
+        logging.error(e)
         return datetime(1970,1,1).isoformat()
 
 def write(i=2):
@@ -85,9 +62,11 @@ def write(i=2):
     client = MongoClient(
         getenv('MONGO_HOST'),
         username = getenv('MONGO_USER') or None,
-        password = getenv('MONGO_PASS') or None
+        password = getenv('MONGO_PASS') or None,
+        ssl = bool(getenv('MONGO_SSL'))
     )
-    collection = client['healthworkers'].messages
+
+    collection = client['healthworkers'].rawmessages
     table = mysql_deets['table']
 
     # Get date of the latest message in our DB
